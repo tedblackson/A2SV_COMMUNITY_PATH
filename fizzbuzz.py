a797af466355a088863d0bfd1499c682f519edbd
Ted_Blackson <62517419+tewodros-web@users.noreply.github.com>
2021-12-15 18:43:30 +0300

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,16 @@
-def fizzbuzz(num):
-    for number in range(1, num+1):
-        if number%3 == 0 and number%5 == 0:
-            print("fizzbuzz")
-        elif number%5 == 0:
-            print("buzz")
-        elif number%3 == 0:
-            print("fizz")
-        else:
-            print(number)
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        answer=[]
+        for i in range(1, n+1):
+            if i%3 == 0 and i%5 == 0:
+                answer[i]= "fizzbuzz"
+            elif i%5 == 0:
+                answer[i] = "buzz"
+            elif i%3 == 0:
+                answer[i]= "fizz"
+            else:
+                answer[i]= str(i)
+        return answer
+            
             
 
