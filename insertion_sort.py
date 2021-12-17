94f762391459451a80f1a8672dbe3f6426c6cfe4
Ted_Blackson <62517419+tewodros-web@users.noreply.github.com>
2021-12-18 00:04:08 +0300

    Update insertion_sort.py

--- insertion_sort.py
+++ insertion_sort.py
@@ -16,23 +16,35 @@ import sys
 
 def insertionSort1(n,arr):
     # Write your code here
-    tmp = arr.pop()
-    arr.append(arr[-1])
-    
-    for i in range(len(arr)-2, -1, -1):
-        if (i+1) < len(arr):
-            if (i+1) < len(arr) and tmp < arr[i]:
-                arr[i+1] = arr[i]
-                if i == 0:
-                    arr[i] = tmp
-                for j in arr:
-                    print(j, end=" ")
-            else: 
-                arr[i+1] = tmp
-                for j in arr:
-                    print(j, end=" ")
-                break
-        print(end="\n")
+    if len(arr)>1:
+        tmp = arr.pop()
+        arr.append(arr[-1])
+
+        for i in range(len(arr)-2, -1, -1):
+            if (i+1) < len(arr):
+                if tmp < arr[i] and i!=0:
+                    arr[i+1] = arr[i]
+                    if i == 0:
+                        arr[i] = tmp
+                    for j in arr:
+                        print(j, end=" ")
+                elif tmp<arr[i] and i==0:
+                    arr[i+1] = arr[i]
+                    for j in arr:
+                        print(j, end=" ")
+                    print(end="\n")
+                    arr[i]=tmp
+                    for j in arr:
+                        print(j, end=" ")
+                    break
+                else: 
+                    arr[i+1] = tmp
+                    for j in arr:
+                        print(j, end=" ")
+                    break
+            print(end="\n")
+    else:
+        print(arr)
 
 if __name__ == '__main__':
     n = int(input().strip())
