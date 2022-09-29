b86765adb122f3565abe4470266522447732074b
tewodros-web <tewodroslm@gmail.com>
2022-09-29 11:21:54 +0300

    one more problem solved

---
+++ HackerRank_Counting_Valleys.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'countingValleys' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER steps
+ *  2. STRING path
+ */
+
+function countingValleys(steps, path) {
+    // Write your code here
+let valleys = 0
+let depth = 0
+let stepNumeric = {"U": 1, "D": -1}
+
+
+for(let step of path){
+    
+    if( depth + stepNumeric[step] < 0 && depth === 0){
+        valleys += 1   
+    }
+        depth += stepNumeric[step]
+    
+    
+}
+    return valleys
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const steps = parseInt(readLine().trim(), 10);
+
+    const path = readLine();
+
+    const result = countingValleys(steps, path);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
