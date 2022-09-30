5594e03bf8395e1b73c38eb9c0e6bb4b2a6d3671
tewodros-web <tewodroslm@gmail.com>
2022-09-30 05:47:54 +0300

    Reduce Array Size To The Half #Array #Sorting #HashTable

---
+++ leetcode_1338.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+ var minSetSize = function(arr) {
+    
+    let counted = new Map()
+    let chosen = new Set()
+    
+    for(let x of arr){
+        
+        if(counted.get(x) === undefined){
+            counted.set(x, 1)
+        }
+        else{
+            counted.set(x, counted.get(x)+1)
+        }
+    }
+    
+    
+    arr.sort((x,y) => y-x)
+    arr.sort((x,y) => {
+        
+        return counted.get(y) - counted.get(x)
+    })
+    
+    let half = arr.length/2
+    chosen.add(arr.shift())
+    
+    while(arr.length > half || chosen.has(arr[0])){
+        chosen.add(arr.shift())
+        
+    }
+    return chosen.size
+    
+    
+    
+};
