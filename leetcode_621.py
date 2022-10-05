24757d93437982f0d21f1a13fb0c72eebea239a5
tewodros-web <tewodroslm@gmail.com>
2022-10-05 10:18:00 +0300

    Task Scheduler #Priority Queue #Heap #Counting

---
+++ leetcode_621.py
@@ -0,0 +1,18 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        
+        counted = Counter(tasks)
+        taskCount = [-count for count in counted.values()]
+        heapq.heapify(taskCount)
+        queue = deque()
+        time = 0
+        
+        while taskCount or queue:
+            time += 1
+            if taskCount:
+                count = heapq.heappop(taskCount) + 1
+                if count:
+                    queue.append([count, time + n])
+            if queue and queue[0][1] == time:
+                    heapq.heappush(taskCount, queue.popleft()[0])
+        return time
