6b50f32d072c955539e9ba47ed4eebb4b7764726
Tewodros Alemu <tewodroslm@gmail.com>
2023-04-08 21:18:05 +0300

    .

---
+++ Leetcode/1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
@@ -0,0 +1,9 @@
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        _max = -1
+        for i in range(len(arr) -1, -1 , -1):
+            temp = arr[i]
+            arr[i] = _max
+            _max = max(temp , _max)            
+        return arr
+        
