321b057d5e66f959754aa139e04554cf55a4c582
Tewodros Alemu <62517419+tedblackson@users.noreply.github.com>
2023-05-18 19:13:56 +0300

    Time: 1816 ms (38.23%), Space: 61.1 MB (81.40%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -1,32 +1,49 @@
 class Solution:
     def minScore(self, n: int, roads: List[List[int]]) -> int:
         
-        graph = defaultdict(list)
-        weights = defaultdict(int)
-        visited = set()
-        
-        for src, dest , weight in roads:
-            graph[src].append(dest)
-            graph[dest].append(src)
-            weights[(min(src, dest), max(src, dest))] = weight
+        self.rep = [i for i in range(n + 1)]
+        self.rank = [1] *(n + 1)
         
         
-        def dfs(root):
+        for x, y , _ in roads:
+            self.union(x, y)
             
-            for child in graph[root]:
-                _min[0] = min(_min[0], weights[(min(root, child), max(root, child))])
-                if child not in visited:
-                    
-                    visited.add(child)
-                    dfs(child)
+        ans = float('inf')
+        
+        for x, y , weight in roads:
+            if self.find(x) == self.find(y) == self.find(1):
+                ans = min(ans, weight )
+        return ans
+                
         
         
-      
-        _min = [float('inf')]
-        visited.add(1)
-        dfs(1)
+    def find(self, x):
         
-        return _min[0]
+        if x == self.rep[x]:
+            return x
+        
+        self.rep[x] = self.find(self.rep[x])
+        
+        return self.rep[x]
+        
+        
+    def union(self, x, y):
+        
+        xrep = self.find(x)
+        yrep = self.find(y)
+        
+        if self.rank[xrep] > self.rank[yrep]:
+            self.rep[yrep] = xrep
+        elif self.rank[xrep] < self.rank[yrep]:
+            self.rep[xrep] = yrep
+        
+        else:
+            self.rep[yrep] = xrep
+            self.rank[xrep] += 1
+        
+        
+        
+
                 
                 
                     
