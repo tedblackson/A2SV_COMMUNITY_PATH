7ef4d0cf85c03363828f999b94a14aeeca1ea66e
Tewodros Alemu <62517419+tedblackson@users.noreply.github.com>
2023-08-05 23:46:38 +0300

    Added solution for Single Number II

---
+++ Leetcode/single-number-ii.py
@@ -0,0 +1,8 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        counted = Counter(nums)
+        
+        for key, val in counted.items():
+            if val == 1 :
+                return key
+            
