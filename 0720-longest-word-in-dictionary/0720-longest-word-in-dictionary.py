044f9cf900a31f8f47dcf40f6cde1a906caaeadc
Tewodros Alemu <62517419+tedblackson@users.noreply.github.com>
2023-10-11 15:57:41 +0300

    Time: 253 ms (8.41%), Space: 17.8 MB (16.10%) - LeetHub

--- 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
@@ -1,22 +1,71 @@
+# class Solution:
+#     def longestWord(self, words: List[str]) -> str:
+        
+#         words_set = set(words)
+        
+#         words.sort( reverse = True)
+#         longest = ""
+#         _len = 0
+#         for word in words:
+            
+#             for i in range(1, len(word) + 1):
+#                 if word[:i] not in words_set:
+#                     break
+#             else:
+#                 if _len <= len(word):
+#                     _len = len(word)
+#                     longest = word
+#         return longest
+                    
+
+
 class Solution:
     def longestWord(self, words: List[str]) -> str:
         
-        words_set = set(words)
+        trie = Trie()
         
-        words.sort( reverse = True)
-        longest = ""
-        _len = 0
         for word in words:
+            trie.addWord(word)
+        
+        longest = ""
+        for word in words:
+            cur = trie.root
             
-            for i in range(1, len(word) + 1):
-                if word[:i] not in words_set:
+            for char in word:
+                idx = ord(char) - ord('a')
+                
+                if not cur.children[idx].is_end:
                     break
+                cur = cur.children[idx]
             else:
-                if _len <= len(word):
-                    _len = len(word)
+                if len(word) > len(longest) or (len(word) == len(longest) and word < longest):
                     longest = word
         return longest
-                    
-                    
-                    
+    
+class TrieNode:
+    
+    def __init__(self,):
+        self.children = [None for _ in range(26)]
+        self.is_end = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+        
+    
+    def addWord(self, word):
+        cur = self.root
+        
+        for char in word:
+            idx = ord(char) - ord('a')
+            
+            if not cur.children[idx]:
+                cur.children[idx] = TrieNode()
+            cur = cur.children[idx]
+        cur.is_end = True
+    
+
+
+        
+    
         
