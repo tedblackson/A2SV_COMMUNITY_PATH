ad99373e5cbd81c028d8a9b668ecfa4d6ae94601
Tewodros Alemu <62517419+tedblackson@users.noreply.github.com>
2023-10-11 15:58:34 +0300

    Time: 248 ms (8.51%), Space: 18.1 MB (7.49%) - LeetHub

--- 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
@@ -1,24 +1,3 @@
-# class Solution:
-#     def longestWord(self, words: List[str]) -> str:
-        
-#         words_set = set(words)
-        
-#         words.sort( reverse = True)
-#         longest = ""
-#         _len = 0
-#         for word in words:
-            
-#             for i in range(1, len(word) + 1):
-#                 if word[:i] not in words_set:
-#                     break
-#             else:
-#                 if _len <= len(word):
-#                     _len = len(word)
-#                     longest = word
-#         return longest
-                    
-
-
 class Solution:
     def longestWord(self, words: List[str]) -> str:
         
