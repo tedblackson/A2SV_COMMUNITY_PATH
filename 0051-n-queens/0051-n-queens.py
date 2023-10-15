497f6153511ec548a1b21d894bed88df3db23884
Tewodros Alemu <62517419+tedblackson@users.noreply.github.com>
2023-10-16 00:12:55 +0300

    Time: 340 ms (7.03%), Space: 16.7 MB (87.00%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.py
@@ -0,0 +1,47 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        ans = []
+        
+        visited = set()
+        board = ["." * n for _ in range(n)]
+        directions = [[1, 0], [0, 1], [-1, 0], [0, -1], [1, 1], [-1, -1], [1, -1], [-1, 1]]
+        
+        def inBound(row, col):
+            return 0 <= row < n and 0 <= col < n
+        
+        def isFree(row, col):
+            
+            for r, c in directions:
+            
+                _row , _col = row, col
+                
+                while inBound(_row, _col):
+                 
+                    if board[_row][_col] == "Q":
+                        return False
+                    _row += r
+                    _col += c
+            return True
+            
+        
+            
+        def dfs(row, board):
+            if row == n:
+                ans.append(board[:])
+                return
+            
+                
+            
+            for col in range(n):
+
+                if isFree(row, col):
+                
+                    board[row] = "." * (col ) + "Q" + "." * (n - col - 1)
+                    dfs(row + 1 , board)
+                    board[row] =  "." * n
+               
+        dfs(0, board)
+        
+       
+        return ans
+                    
